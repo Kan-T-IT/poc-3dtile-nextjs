47b34723f9f7e1dbfde4091acd97142616bb11a1
Sebastian Oscar Lopez <sebastianoscarlopez@gmail.com>
2023-12-10 13:58:22 -0300

    feat: 3d tile based on https://github.com/birkskyum/deckgl-summit-4-big-deckgl-iconlayer-interleaved-into-maplibre

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
+import { MapboxLayer } from '@deck.gl/mapbox';
+import {Tiles3DLoader} from '@loaders.gl/3d-tiles';
+import {Tile3DLayer} from '@deck.gl/geo-layers';
+
 import styles from './page.module.css'
 import { useState, useEffect } from "react";
 
@@ -16,7 +20,7 @@ export default function Home() {
       style: {
         version: 8,
         sources: {
-          "baseCepal": {
+          "baseIgn": {
             type: "raster",
             scheme: "tms",
             tiles: [
@@ -27,16 +31,16 @@ export default function Home() {
         },
         layers: [
           {
-            id: "baseCepal",
+            id: "baseIgn",
             type: "raster",
-            source: "baseCepal",
+            source: "baseIgn",
             minzoom: 0,
             maxzoom: 19,
           },
         ],
       },
       hash: true,
-      center: [-66.336888, -33.301862],
+      center: [ -70.9055384313, -39.2364536512 ],
       zoom: 16,
     });
     setMap(mapInstance);
@@ -49,10 +53,35 @@ export default function Home() {
     if(map) {
       map.on('load', async () => {
         console.log('load');
-        // map.addLayer(
-        //   await DeckGL_IconLayer(),
-        //   MapTilerAPIKey.length > 0 ? 'road_pier' : ''
-        // );
+        const tile3dLayer = await new MapboxLayer({
+          id: 'tile-3d-layer',
+          type: Tile3DLayer,
+          data: 'https://mesh3d.s3.eu-west-2.amazonaws.com/neuquen/b3dm_1_0-200/tileset.json',
+          loader: Tiles3DLoader,
+          onTilesetLoad: (tileset) => {
+            console.log('tileset', tileset);
+            // Recenter to cover the tileset
+            const {cartographicCenter, zoom} = tileset;
+            // this.setState({
+            //     viewState: {
+            //       ...this.state.viewState,
+            //       longitude: cartographicCenter[0],
+            //       latitude: cartographicCenter[1],
+            //       zoom
+            //     }
+            // });
+            console.log('cartographicCenter', cartographicCenter, deck);
+            // goToMesh();
+          },
+          // override scenegraph subLayer prop
+          _subLayerProps: {
+            scenegraph: {_lighting: 'flat'}
+          }
+        });
+      
+        map.addLayer(
+          tile3dLayer
+        );
       });
     }
   }, [map]);
